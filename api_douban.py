b6ee1315e2e38b981a3488d16b50b549fd88f442
fangplus <fangjiageek@gmail.com>
2017-04-01 00:04:14 +0800

    Add files via upload

---
+++ api_douban.py
@@ -0,0 +1,14 @@
+#---coding:utf-8---
+import urllib2
+import json
+ranking = 1
+htmls = ['https://api.douban.com/v2/movie/top250?start={}'.format((page-1)*20)for page in range(1,6)]
+for html in htmls:
+    try:
+        web_data = urllib2.urlopen(html)
+        hjson = json.loads(web_data.read())
+    except Exception as error:
+        print error
+    for key in hjson['subjects']:
+        print str(ranking) + ' : ' + key['title'] + ',' + key['year'] + ',' + str(key['rating']['average'])
+        ranking += 1
